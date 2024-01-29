1de172ff917b6c584953c265d7f690ea3c09dbf4
Alcadeus <aazamthakur@gmail.com>
2024-01-29 14:39:00 +0530

    added graphql

--- blog.js
+++ blog.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!");
+const { request } = require('graphql-request')
+
+const query = `
+query Publication {
+ publication(host: "alcadeus.hashnode.dev") {
+    isTeam
+    title
+    posts(first: 10) {
+      edges {
+        node {
+          title
+          brief
+          url
+        }
+      }
+    }
+ }
+}
+`
+
+request('https://api.example.com/graphql', query)
+ .then((data) => console.log(data))
